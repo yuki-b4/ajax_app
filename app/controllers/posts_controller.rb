c648ecb507c3d68d5e3bb324329c298d1ce7ba02
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-17 15:57:40 +0900

    メモ投稿機能のサーバーサイドの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@
     end
 
     def create
-      Post.create(content: params[:content])
-      redirect_to action: :index
+      post = Post.create(content: params[:content], checked: false)
+      render json:{post: post}
     end
 
     def checked
